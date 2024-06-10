79892db67b1699633a5332a5598e4ff9b7b1bb15
dev91junior <devjk91@outlook.com>
2024-06-10 16:21:28 +0200

    Checking Winning Combinations

--- assets/main.js
+++ assets/main.js
@@ -22,13 +22,54 @@ cells.forEach((cell, index) =>
             board[index] = currentPlayer;
             cell.textContent = currentPlayer;
 
-            // Vérifier si un joueur a gagné ou si c'est une partie nulle
-            // checkWinner();
-
-
             // Changer de joueur
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+            // Vérifier si un joueur a gagné ou si c'est une partie nulle
+            checkWinner();
         }
     })
 );
 
+
+// tableau des combinaison gagnant 
+const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // lignes
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // colonnes
+    [2, 4, 6], [0, 4, 8] // diagonales
+];
+
+function checkWinner() {
+    // vérifier que les combinaisons gagnante pour le joueur 'X'
+    for (let combination of winningCombinations) {
+        if(
+            board[combination[0]] === 'X' &&
+            board[combination[1]] === 'X' &&
+            board[combination[2]] === 'X' 
+        ) {
+            alert('le joueur X a gagné !');
+            gameOver = true;
+            return;
+        };
+    };
+
+    // vérifier que les combinaisons gagnante pour le joueur 'O'
+    for (let combination of winningCombinations) {
+        if(
+            board[combination[0]] === 'O' &&
+            board[combination[1]] === 'O' &&
+            board[combination[2]] === 'O' 
+        ) {
+            alert('le joueur O a gagné !');
+            gameOver = true;
+            return;
+        };
+    };
+
+    // vérifier si une partie est null
+    if (!board.includes('')) {
+        alert("c'est une partie nulle")
+        gameOver = true
+        
+    };
+};
